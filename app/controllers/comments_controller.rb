0935c81215e5f9ae1dca3daf7c2776e762da38c1
t.vieira <t.vieira@global.leaseweb.com>
2024-07-28 20:05:19 +0100

    Add Posts and comment scaffold

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    if @comment.save
+      redirect_to @post, notice: 'Comment was successfully created.'
+    else
+      redirect_to @post, alert: 'Failed to create comment.'
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @post, notice: 'Comment was successfully destroyed.'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'posts#index'
 end
